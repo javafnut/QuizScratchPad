923918301f9349e85e62450ff62a2472409d18ec
Todd Johnston <toddjohnston.ibexsys@gmail.com>
2017-02-13 20:23:07 -0800

    Commit message

---
+++ src/com/ibexsys/lambda/LambdaFooBar.java
@@ -0,0 +1,12 @@
+package com.ibexsys.lambda;
+
+/**
+ * Created by tj on 2/13/17.
+ */
+
+public class LambdaFooBar {
+
+    public static void main(String[] args) {
+        System.out.println("Lambda FooBar");
+    }
+}
